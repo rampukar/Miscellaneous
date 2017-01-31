07918fbae1abff46cadd0b5da9e58563006ea809
Ram Pukar <ram_mosone@yahoo.com>
2017-01-31 15:16:30 +0545

    FIXES :: Static variable change.

--- ConvertToRoman.php
+++ ConvertToRoman.php
@@ -1,19 +1,19 @@
 <?php 
 class ConvertToRoman {
-    private $decimalNums    =[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    private $romanNums    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    private static $decimalNums    =[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    private static $romanNums    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
-    public function getConvert($argNum = null) {
+    public static function getConvert($argNum = null) {
         $romanVal = '';
-        for ($index = 0; $index < count($this->decimalNums); $index++) {
-            while ($this->decimalNums[$index] <= $argNum) {
-               $romanVal.= $this->romanNums[$index];
-               $argNum-=$this->decimalNums[$index];
+        for ($index = 0; $index < count(self::$decimalNums); $index++) {
+            while (self::$decimalNums[$index] <= $argNum) {
+               $romanVal.= self::$romanNums[$index];
+               $argNum-=self::$decimalNums[$index];
            }
        }
        return $romanVal;
    }
 }
-$getValue = new ConvertTwoRoman();
-var_dump($getValue->getConvert(101));
+$getValue = ConvertToRoman::getConvert(101);
+var_dump($getValue);
 ?>
